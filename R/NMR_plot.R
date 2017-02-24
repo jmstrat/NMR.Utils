88dafed7eda322282b5fc0a0ee6d95cc5ab0ac5f
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-02-24 20:57:04 +0000

    Use plotting.utils for colour scaling function

--- R/NMR_plot.R
+++ R/NMR_plot.R
@@ -91,8 +91,8 @@ plotNMR2D <-function(nmrdata,xrange,yrange,plot_offset,plot.colour=TRUE,plot.col
           cvar=signif(cvar,5)
           cvar[cvar<cr[[1]]]<-cr[[1]]
           cvar[cvar>cr[[2]]]<-cr[[2]]
-
-          cols=color.scale.nmr(cvar,c(0,0,0,1,1,1),c(0,0,1,1,0,0),c(0,1,0,0,1,0),xrange=cr)
+          load_or_install(Plotting.Utils)
+          cols=color.scale.jms(cvar,c(0,0,0,1,1,1),c(0,0,1,1,0,0),c(0,1,0,0,1,0),xrange=cr)
           nseg=length(prx)-1
           segments(prx[1:nseg], pry[1:nseg], prx[2:(nseg + 1)], pry[2:(nseg + 1)], col = cols,lwd=lwd)
           #tryCatch(color.scale.lines(prx,pry,c(0,0,0,1,1,1),c(0,0,1,1,0,0),c(0,1,0,0,1,0),colvar=cvar,lwd=lwd), error = function(e) lines(x,y,col=col,lwd=plot_line_lwd))
@@ -158,121 +158,4 @@ plotNMR2D <-function(nmrdata,xrange,yrange,plot_offset,plot.colour=TRUE,plot.col
     #return alignment parameters
     invisible(list(time_scan_1=offsets[[1]], offset_scan_1=pos[[1]],time_scan_last=offsets[[n-1]], offset_scan_last=pos[[n-1]], yrange=yrange))
 
-}
-
-#' Colour NMR Data
-#'
-#' This function is a workaround for an issue with \code{\link[plotrix]{color.scale}}.
-#' @keywords internal
-color.scale.nmr <- function (x, cs1 = c(0, 1), cs2 = c(0, 1), cs3 = c(0, 1), alpha = 1, extremes = NA, na.color = NA, xrange = NULL, color.spec = "rgb") {
-  naxs <- is.na(x)
-  if (!is.na(extremes[1])) {
-    colmat <- col2rgb(extremes)
-    cs1 <- colmat[1, ]/255
-    cs2 <- colmat[2, ]/255
-    cs3 <- colmat[3, ]/255
-    color_spec <- "rgb"
-  }
-  maxcs1 <- ifelse(color.spec == "hcl", 360, 1)
-  maxcs2 <- ifelse(color.spec == "hcl", 100, 1)
-  maxcs3 <- ifelse(color.spec == "hcl", 100, 1)
-  #ncolors <- length(x)
-  if (is.null(xrange)) {
-    xrange <- range(x, na.rm = TRUE)
-    drop.extremes <- FALSE
-  } else {
-    if (xrange[1] > min(x, na.rm = TRUE) || xrange[2] < max(x,
-                                                            na.rm = TRUE))
-      stop("An explicit range for x must include the range of x values.")
-    x <- c(xrange, x)
-    drop.extremes = TRUE
-  }
-  ncolors <- length(x) ##MOVED THIS TO HERE
-  ncs1 <- length(cs1)
-  if (ncs1 > 1) {
-    cs1s <- rep(cs1[ncs1], ncolors)
-    xstart <- xrange[1]
-    xinc <- diff(xrange)/(ncs1 - 1)
-    for (seg in 1:(ncs1 - 1)) {
-      segindex <- which((x >= xstart) & (x <= (xstart +
-                                                 xinc)))
-      cs1s[segindex] <- rescale.nmr(x[segindex], cs1[c(seg,
-                                                   seg + 1)])
-      xstart <- xstart + xinc
-    }
-    if (min(cs1s, na.rm = TRUE) < 0 || max(cs1s, na.rm = TRUE) >
-        maxcs1)
-      cs1s <- rescale.nmr(cs1s, c(0, maxcs1))
-  } else cs1s <- rep(cs1, ncolors)
-  ncs2 <- length(cs2)
-  if (ncs2 > 1) {
-    cs2s <- rep(cs2[ncs2], ncolors)
-    xstart <- xrange[1]
-    xinc <- diff(xrange)/(ncs2 - 1)
-    for (seg in 1:(ncs2 - 1)) {
-      segindex <- which((x >= xstart) & (x <= (xstart +
-                                                 xinc)))
-      cs2s[segindex] <- rescale.nmr(x[segindex], cs2[c(seg,
-                                                   seg + 1)])
-      xstart <- xstart + xinc
-    }
-    if (min(cs2s, na.rm = TRUE) < 0 || max(cs2s, na.rm = TRUE) >
-        maxcs2)
-      cs2s <- rescale.nmr(cs2s, c(0, maxcs2))
-  } else cs2s <- rep(cs2, ncolors)
-  ncs3 <- length(cs3)
-  if (ncs3 > 1) {
-    cs3s <- rep(cs3[ncs3], ncolors)
-    xstart <- xrange[1]
-    xinc <- diff(xrange)/(ncs3 - 1)
-    for (seg in 1:(ncs3 - 1)) {
-      segindex <- which((x >= xstart) & (x <= (xstart +
-                                                 xinc)))
-      cs3s[segindex] <- rescale.nmr(x[segindex], cs3[c(seg,
-                                                   seg + 1)])
-      xstart <- xstart + xinc
-    }
-    if (min(cs3s, na.rm = TRUE) < 0 || max(cs3s, na.rm = TRUE) >
-        maxcs3)
-      cs3s <- rescale.nmr(cs3s, c(0, maxcs3))
-  } else cs3s <- rep(cs3, ncolors)
-  if (drop.extremes) {
-      cs1s <- cs1s[-(1:2)]
-      cs2s <- cs2s[-(1:2)]
-      cs3s <- cs3s[-(1:2)]
-  }
-  xdim <- dim(x)
-  colors <- do.call(color.spec, list(cs1s, cs2s, cs3s, alpha = alpha))
-  if (!is.null(xdim))
-    colors <- matrix(colors, nrow = xdim[1])
-  if (length(naxs))
-    colors[naxs] <- na.color
-  return(colors)
-}
-
-#' Rescale Data
-#'
-#' This function is a workaround for an issue with \code{\link[plotrix]{rescale}}.
-#' @keywords internal
-rescale.nmr <- function (x, newrange)
-{
-  if (missing(x) | missing(newrange)) {
-    usage.string <- paste("Usage: rescale(x,newrange)\n", "\twhere x is a numeric object and newrange is the new min and max\n", sep = "", collapse = "")
-    stop(usage.string)
-  }
-  if (is.numeric(x) && is.numeric(newrange)) {
-    xna <- is.na(x)
-    if (all(xna))
-      return(x)
-    if (any(xna))
-      xrange <- range(x[!xna])
-    else xrange <- range(x)
-    if (xrange[1] == xrange[2])
-      return(rep_len(newrange[[1]],length(x)))
-    mfac <- (newrange[2] - newrange[1])/(xrange[2] - xrange[1])
-    return(newrange[1] + (x - xrange[1]) * mfac)
-  } else {
-    warning("Only numeric objects can be rescaled")
-    return(x)
-  }
 }
