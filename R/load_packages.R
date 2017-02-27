0eb9a1f19ca2fe610c881741ca191773d72f6359
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-02-27 16:37:31 +0000

    Update to use newer version of load_or_install

--- R/load_packages.R
+++ R/load_packages.R
@@ -1,17 +1,31 @@
 #' Load packages
 #'
 #' This function will load a package, or install it if it isn't present. (Probably against all sorts of programming guidelines, but makes everything easier to install and run...)
-#' @param x A (complex) dataframe of NMR data
-#' @return None
+#' @param package The package name
+#' @param optional If FALSE then an error will be raised if the package cannot be loaded
+#' @param github If installing from cran fails try this github repository instead (see \code{\link[devtools]{install_github}})
+#' @return TRUE if the package was successfully loaded. FALSE if the package was unable to be loaded and optional=TRUE. Raises an error otherwise.
 #' @examples
 #' load_or_install("baseline")
 #' @keywords internal
-load_or_install <- function(x)
+load_or_install <- function(package,optional=FALSE, github=NA)
 {
-  if (!require(x,character.only = TRUE))
+  if (!require(package,character.only = TRUE))
   {
-    warning(paste0('The package "',x,'" was not found, attempting to install...'))
-    install.packages(x,dep=TRUE)
-    if(!require(x,character.only = TRUE)) stop("Package not found")
+    flog.warn('The package "%s was not found, attempting to install...',package)
+    install.packages(package,dependencies=TRUE, repos="http://cran.rstudio.com/")
+    if(!require(package,character.only = TRUE)) {
+      #Check if available on github
+      if(!is.na(github)) {
+        flog.warn('Package "%s was not found on cran, checking github...',package)
+        if(load_or_install('devtools',optional=TRUE) && tryCatch({devtools::install_github(github)
+          return(TRUE)}, error = function(e) return(FALSE))) {
+          if(require(package,character.only = TRUE)) return(invisible(TRUE))
+        }
+      }
+      if(!optional) stop("Package '",package,"' not found",call. = FALSE) else flog.warn('The optional package "%s was not found.',package)
+      return(invisible(FALSE))
+    }
   }
+  return(invisible(TRUE))
 }
