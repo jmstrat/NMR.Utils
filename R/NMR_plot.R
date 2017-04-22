037891c77d211217a23de6cb1bd722ee35207ae2
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-04-22 16:49:30 +0100

    Improve 2D plot with version from Sn paper

--- R/NMR_plot.R
+++ R/NMR_plot.R
@@ -7,7 +7,7 @@
 #' @param plot_offset offset between subsequent plots
 #' @param xTickInterval Tick interval for x axis
 #' @param xMinorTickInterval Minor tick interval for x axis
-#' @param y_trunc Truncate y values that are higher than this
+#' @param y_trunc Truncate y values that are higher than this (or NA for auto)
 #' @param col Line colour for plot
 #' @param lwd Line width for plot
 #' @param shade_under TRUE / FALSE shade under the plot
@@ -31,141 +31,158 @@
 #' plotNMR2D(data, xrange=c(1500,-500), yrange='auto', plot_offset=100000, plot.colour.ranges=list(c(210,900)), plot.colour.yranges=list(c(50000,200000)))
 plotNMR2D <-function(nmrdata,xrange,yrange,plot_offset,plot.colour=TRUE,plot.colour.ranges=NA,plot.colour.yranges='auto', xTickInterval=200,xMinorTickInterval=50,y_trunc=NA,col='Black',lwd=1,shade_under=FALSE,shade_col='grey',y_trunc_x_points=c(),y_trunc_amp_div=200,y_trunc_label_offset_factor=20,y_trunc_sin_period=5,y_trunc_labels=c(),y_trunc_text_col='grey',y_trunc_line_col='grey',y_trunc_lwd=2,show_axes=TRUE,show_RH_Tick=TRUE) {
   load_or_install("plotrix")
-    n=ncol(nmrdata)
-    x=nmrdata[,1] #ppm
-    offsets=as.numeric(names(nmrdata)[2:length(nmrdata)])
-    #convert offsets in hours to plot coordinates
-    pos=offsets*plot_offset
+  load_or_install('Plotting.Utils')
+  n=ncol(nmrdata)
+  x=nmrdata[,1] #ppm
+  offsets=as.numeric(names(nmrdata)[2:n])
 
-    if(any(yrange=='auto'))
-    {
-      yrangemin=min(nmrdata[,2]+pos[[1]])
-      yrangemax=max(nmrdata[,n]+pos[[n-1]])
-      yrange<-c(yrangemin,yrangemax)
+  #Subsetting drops attributes, but we only care about the nucleus
+  nucleus=attr(nmrdata,'nuc1')
+  if(is.null(nucleus)) nucleus='Unknown Nucleus'
+
+  #Remove scans where offset = NA
+  nmrdata=nmrdata[,c(TRUE,!is.na(offsets))]
+  offsets=offsets[!is.na(offsets)]
+  n=ncol(nmrdata)
+  #convert offsets in hours to plot coordinates
+  pos=offsets*plot_offset
+
+  if(any(yrange=='auto'))
+  {
+    yrangemin=min(nmrdata[,2]+pos[[1]])
+    yrangemax=max(nmrdata[,n]+pos[[n-1]])
+    yrange<-c(yrangemin,yrangemax)
+  }
+
+  if(is.na(y_trunc))
+    y_trunc=15/16*yrange[[2]]
+
+  #plot (blank plot)
+  new_plot(xrange,yrange)
+
+  if(plot.colour) {
+    if(any(is.na(plot.colour.ranges))) {
+      plot.colour.ranges=list(xrange)
     }
+    col_ranges=plot.colour.yranges
+    if(any(col_ranges=='auto')) {
+      col_ranges=list()
+      for(r in plot.colour.ranges) {
+        crange=c(Inf,-Inf)
+        for(i in n:2) {
+          cy=nmrdata[,i][x<r[[1]]&x>r[[2]]]
+          mincy=min(cy)
+          maxcy=max(cy)
+          if(mincy<crange[[1]]) crange[[1]]=signif(mincy,5)
+          if(maxcy>crange[[2]]) crange[[2]]=signif(maxcy,5)
+        }
+        col_ranges=append(col_ranges,list(crange))
+      }
+    }
+  }
 
-    if(is.na(y_trunc))
-      y_trunc=15/16*yrange[[2]]
-
-    #plot (blank plot)
-    plot(0,0, type="n", xaxs="i", yaxs="i", xlim=xrange, ylim=yrange, axes = FALSE, xlab = NA, ylab = NA)
-
+  #loop over scans to plot (reverse order)
+  for(i in n:2) {
+    #calculate offset for scan
+    off=pos[[(i-1)]]
+    #add offset to intensity
+    y=nmrdata[,i]+off
+    #plot scan
     if(plot.colour) {
-      if(any(is.na(plot.colour.ranges))) {
-        plot.colour.ranges=list(xrange)
+      allx=x
+      ally=y
+      for(r in 1:length(plot.colour.ranges)) {
+        prx=plot.colour.ranges[[r]]
+        pry=y[x<prx[[1]]&x>prx[[2]]]
+        prx=x[x<prx[[1]]&x>prx[[2]]]
+
+        ally[allx %in% prx] <- NA
+
+        cr=col_ranges[[r]]
+
+        cvar=pry-off
+        cvar=signif(cvar,5)
+        cvar[cvar<cr[[1]]]<-NA
+        cvar[cvar>cr[[2]]]<-NA#cr[[2]]
+
+        cols=color.scale.jms(cvar,c(0,0,1,1,1),c(0,1,1,0,0),c(1,0,0,1,0),xrange=cr,na.color='black')
+
+        nseg=length(prx)-1
+        segments(prx[1:nseg], pry[1:nseg], prx[2:(nseg + 1)], pry[2:(nseg + 1)], col = cols,lwd=lwd)
       }
-      col_ranges=plot.colour.yranges
-      if(any(col_ranges=='auto')) {
-        col_ranges=list()
-        for(r in plot.colour.ranges) {
-          crange=c(Inf,-Inf)
-          for(i in n:2) {
-            cy=nmrdata[,i][x<r[[1]]&x>r[[2]]]
-            mincy=min(cy)
-            maxcy=max(cy)
-            if(mincy<crange[[1]]) crange[[1]]=signif(mincy,5)
-            if(maxcy>crange[[2]]) crange[[2]]=signif(maxcy,5)
-          }
-          col_ranges=append(col_ranges,list(crange))
-        }
+
+      nseg=length(allx)
+      segments(allx[1:nseg], ally[1:nseg], allx[2:(nseg + 1)], ally[2:(nseg + 1)], col = col,lwd=lwd)
+    } else {
+      lines(x,y,col=col,lwd=lwd)
+    }
+    if(shade_under){
+      #add shading
+      polygon(c(x,x[length(x)]), c(y, y[1]), col=shade_col,border=NA)
+    }
+  }
+
+  #loop over truncation lines
+  if(length(y_trunc_x_points)>0){
+    for(i in 1:length(y_trunc_x_points)) {
+
+      #calculate parameters for sine wave
+      xs=y_trunc_x_points[[i]]
+      xcent=mean(xs)
+      ycent=y_trunc/y_trunc_amp_div+y_trunc+y_trunc/y_trunc_label_offset_factor
+
+      #calculate parameters for white fill region
+      xs3=seq(xs[[1]], xs[[2]], length.out = 101)
+      ys3=lapply(xs3,function(x) sin((x-xs[[1]])*pi/y_trunc_sin_period)*y_trunc/y_trunc_amp_div+y_trunc)
+      xs3=append(xs3,c(xs[[2]],xs[[1]]))
+      ys3=append(ys3,c(yrange[[2]],yrange[[2]]))
+      polygon(xs3,ys3,col='white',border=NA)
+
+      #draw truncation line
+      curve(sin((x-xs[[1]])*pi/y_trunc_sin_period)*y_trunc/y_trunc_amp_div+y_trunc,from=xs[[1]],to=xs[[2]],col=y_trunc_line_col,add=T,lwd=y_trunc_lwd)
+      #add label
+      #Note reversed axis == negative string width!!
+      if(xcent-(strwidth(y_trunc_labels[i])+strwidth(y_trunc_labels[i])/2*0.1)/2>xrange[[1]]) {
+        xcent=xrange[[1]]+strwidth(y_trunc_labels[i])/2+strwidth(y_trunc_labels[i])/2*0.1
+      } else if(xcent+(strwidth(y_trunc_labels[i])+strwidth(y_trunc_labels[i])/2*0.1)/2<xrange[[2]]) {
+        xcent=xrange[[2]]-strwidth(y_trunc_labels[i])/2-strwidth(y_trunc_labels[i])/2*0.1
       }
+      text(xcent,ycent,y_trunc_labels[i],col=y_trunc_text_col)
+    }
+  }
+  #make plot pretty
+  box()
+
+  if(show_axes) {
+    ticksat=seq(xrange[[1]],xrange[[2]],xTickInterval*sign(xrange[[2]]-xrange[[1]]))
+    minorTicksat=seq(xrange[[1]],xrange[[2]],xMinorTickInterval*sign(xrange[[2]]-xrange[[1]]))
+
+    minorTicksat=minorTicksat[!(minorTicksat %in% ticksat)]
+
+    #Add minor ticks
+    axis(side = 1, tcl = -.2, lwd=0,lwd.ticks=1,at=minorTicksat, labels = NA)
+    #Add major ticks
+    axis(side = 1, tcl = -.4, lwd=0,lwd.ticks=1,at=ticksat, labels = NA)
+
+    ##Add axis With labels (With reduced spacing from axis -- line=.4)
+    lab = ticksat
+    if(!show_RH_Tick)
+      if(xrange[[2]]==lab[[length(lab)]])
+        lab[[length(lab)]]=''
+    axis(side = 1, lwd = 0, tcl = -0.5, line = -.8, las = 1,at=ticksat,labels=lab)
+
+    if(nucleus=='Unknown Nucleus') {
+      axis_label=expression('Chemical Shift / ppm')
+    } else {
+      element=sub('[0-9]*([A-Za-z]*)','\\1',nucleus)
+      mass_number=sub('([0-9]*)[A-Za-z]*','\\1',nucleus)
+      axis_label=parse(text=paste0('"δ ("^"',mass_number,'"*"',element,') / ppm"'))
     }
 
-    #loop over scans to plot (reverse order)
-    for(i in n:2) {
-      #calculate offset for scan
-      off=pos[[(i-1)]]
-      #add offset to intensity
-      y=nmrdata[,i]+off
-      #truncate points (add 20% to remove artefacts...)
-      #y[y>y_trunc+y_trunc/5] <- NA
-      #plot scan
-      if(plot.colour) {
-        allx=x
-        ally=y
-        for(r in 1:length(plot.colour.ranges)) {
-          prx=plot.colour.ranges[[r]]
-          pry=y[x<prx[[1]]&x>prx[[2]]]
-          prx=x[x<prx[[1]]&x>prx[[2]]]
-
-          ally[allx %in% prx] <- NA
-
-          cr=col_ranges[[r]]
-
-          cvar=pry-off
-          cvar=signif(cvar,5)
-          #cvar[cvar<cr[[1]]]<-cr[[1]]
-          #cvar[cvar>cr[[2]]]<-cr[[2]]
-          load_or_install('Plotting.Utils')
-          cvar[cvar<cr[[1]]]<-NA
-          cvar[cvar>cr[[2]]]<-NA#cr[[2]]
-
-          cols=color.scale.jms(cvar,c(0,0,1,1,1),c(0,1,1,0,0),c(1,0,0,1,0),xrange=cr,na.color='black')
-
-          #cols=color.scale.jms(cvar,c(0,0,0,1,1,1),c(0,0,1,1,0,0),c(0,1,0,0,1,0),xrange=cr)
-          nseg=length(prx)-1
-          segments(prx[1:nseg], pry[1:nseg], prx[2:(nseg + 1)], pry[2:(nseg + 1)], col = cols,lwd=lwd)
-          #tryCatch(color.scale.lines(prx,pry,c(0,0,0,1,1,1),c(0,0,1,1,0,0),c(0,1,0,0,1,0),colvar=cvar,lwd=lwd), error = function(e) lines(x,y,col=col,lwd=plot_line_lwd))
-        }
-        nseg=length(allx)
-        segments(allx[1:nseg], ally[1:nseg], allx[2:(nseg + 1)], ally[2:(nseg + 1)], col = col,lwd=lwd)
-        #lines(allx,ally,col=col,lwd=lwd)
-      } else {
-        lines(x,y,col=col,lwd=lwd)
-      }
-      if(shade_under){
-        #add shading
-        polygon(c(x,x[length(x)]), c(y, y[1]), col=shade_col,border=NA)
-      }
-    }
-
-    #loop over truncation lines
-    if(length(y_trunc_x_points)>0){
-      for(i in 1:length(y_trunc_x_points)) {
-
-        #calculate parameters for sine wave
-        xs=y_trunc_x_points[[i]]
-        xcent=mean(xs)
-        ycent=y_trunc/y_trunc_amp_div+y_trunc+y_trunc/y_trunc_label_offset_factor
-
-        #calculate parameters for white fill region
-        xs2=c(xs[[1]],xs)
-        xs2=append(xs2,xs[[length(xs)]])
-        ys2=c(yrange[[2]],y_trunc,y_trunc,yrange[[2]])
-        #fill with white
-        polygon(xs2,ys2,col="white",border=NA)
-
-        #draw truncation line
-        curve(sin(x*pi/y_trunc_sin_period)*y_trunc/y_trunc_amp_div+y_trunc,from=xs[[1]],to=xs[[2]],col=y_trunc_line_col,add=T,lwd=y_trunc_lwd)
-        #add label
-        text(xcent,ycent,y_trunc_labels[i],col=y_trunc_text_col)
-      }
-    }
-    #make plot pretty
-    box()
-
-    if(show_axes) {
-        ticksat=seq(xrange[[1]],xrange[[2]],xTickInterval*sign(xrange[[2]]-xrange[[1]]))
-        minorTicksat=seq(xrange[[1]],xrange[[2]],xMinorTickInterval*sign(xrange[[2]]-xrange[[1]]))
-
-        #Add axis with no labels
-        #axis(side = 2, tck = -.015, tick=FALSE, labels=NA)
-        axis(side = 1, tck = -.015, labels = NA,at=ticksat)
-        axis(side = 1, tck = -.005, labels = NA,at=minorTicksat)
-
-        ##Add axis With labels (With reduced spacing from axis -- line=.4)
-        #axis(side = 2, lwd = 0, line = -.4, tick=FALSE, labels=NA)
-        lab = ticksat
-        if(!show_RH_Tick)
-          if(xrange[[2]]==lab[[length(lab)]])
-            lab[[length(lab)]]=''
-        axis(side = 1, lwd = 0, line = -.4, las = 1,at=ticksat,labels=lab)
-
-        ##Add axis titles
-        mtext(side = 2, "Intensity", line = 1)
-        mtext(side = 1, "ppm", line = 1.8)
-    }
-    #return alignment parameters
-    invisible(list(time_scan_1=offsets[[1]], offset_scan_1=pos[[1]],time_scan_last=offsets[[n-1]], offset_scan_last=pos[[n-1]], yrange=yrange))
+    ##Add axis titles
+    mtext(side = 1, axis_label, line = 1.1)
+  }
+  #return alignment parameters
+  invisible(list(time_scan_1=offsets[[1]], offset_scan_1=pos[[1]],time_scan_last=offsets[[n-1]], offset_scan_last=pos[[n-1]], yrange=yrange))
 
 }
