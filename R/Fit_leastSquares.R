7c49e24e4ad0c912d7be868e769d56a2dfca9226
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-04-28 14:48:19 +0100

    Fix use of estimation function, and formatting

--- R/Fit_leastSquares.R
+++ R/Fit_leastSquares.R
@@ -9,25 +9,25 @@
 #' run_fit_for_data(fit, data)
 run_fit_for_data <-function(fit,data) {
   load_or_install("minpack.lm")
-  
+
 	#Number of scans to fit = number of columns - 1 (first col == x values)
-	nc=ncol(data)  
-  
+	nc=ncol(data)
+
 	if(fit$determine_parameters_dynamically) {
     warning("Automatic parameter calculation has not been re-written for this library yet... (& it may never be)")
 		#TODO:
     #run function to determine parameters here...
 	}
-  
+
   fit=fix_unconstrained_parameters(fit)
   fit=prepare_result_data_frame(fit,nc-1)
-  
+
   progress=time_remaining_start(nc-1)
   #Begin loop over scans
   for(i in 2:nc) {
     fit=fit_single_scan(fit, data[,c(1,i)])
     fit=integrate_last_scan(fit,data[,c(1,i)])
-    
+
     fit$last_scan=i-1
     progress(i-1)
   }
@@ -47,7 +47,7 @@ fit_single_scan <- function(fit, data) {
   n_models=length(fit$models)
   x=data[,1]
   y=data[,2]
-  
+
   #ignore data outside the integration range
   y[x>fit$integration_range[[2]]]<-NA
   y[x<fit$integration_range[[1]]]<-NA
@@ -55,14 +55,14 @@ fit_single_scan <- function(fit, data) {
   for(region in fit$ppm_exclude) {
     y[x<region[[2]]&x>region[[1]]]<-NA
   }
-  
+
   sn=fit$last_scan
   nModel=length(fit$models)
 
   #Update models
   for(m in 1:nModel) {
     model=fit$models[[m]]
-    
+
     #Get previous values for model parameters
     if(sn>0) {
       vars=names(model$current_guess)
@@ -78,10 +78,10 @@ fit_single_scan <- function(fit, data) {
       #We need to update the current guess to the values from the last fit
       model$current_guess=previous_values
     }
-    if(!is.na(model$estimation_function)) {
+    if(is.function(model$estimation_function)) {
       #Call the estimation function to update values
       estimate=model$estimation_function(i,model_list[[m]]$guess,c(x,y))
-      estimate=estimate(is.numeric(estimate))
+      estimate=estimate[is.numeric(unlist(estimate))]
       vars=names(estimate)
       model$current_guess[vars]=estimate
     }
@@ -104,7 +104,7 @@ fit_single_scan <- function(fit, data) {
     model$upper[upper_lim]<-unlist(sapply(model$constraint[upper_lim], "[[", 3))
     lower_lim=names(model$constraint[vr_constraints][model$lower[vr_constraints]<sapply(model$constraint[vr_constraints], "[[", 2)])
     model$lower[lower_lim]<-unlist(sapply(model$constraint[lower_lim], "[[", 2))
-    
+
     #Fix any ordering issues:
     model$lower=model$lower[names(model$constraint)]
     model$upper=model$upper[names(model$constraint)]
@@ -113,7 +113,7 @@ fit_single_scan <- function(fit, data) {
   }
   #Determine control values
   control=nls.lm.control(maxiter=fit$maxIterations,epsfcn=fit$noise_height,maxfev=fit$maxEvaluations)
-  
+
   #For saving results:
   best_r2=Inf
   best_fit=NA
@@ -135,11 +135,11 @@ fit_single_scan <- function(fit, data) {
           #Run the fit
           fit_result=fit_models_to_x_y(current_models,x,y, control)
         }
-      
+
         #Check if it failled
         if(all(is.na(fit_result)))
           next
-      
+
         #If it didn't fail, calculate r^2 and compare to previous attempts
         r2=sqrt(sum(residuals(fit_result)^2))
         if(r2 < best_r2) {
@@ -161,7 +161,7 @@ fit_single_scan <- function(fit, data) {
   #Extract parameters:
   fitted_parameters=coef(best_fit)
   #BEWARE: model numbers from fit result will NOT be the same as in the fit object (must convert using best_models)
-  
+
   #Convert unique variables from fit back to model variables and store in results table
   fitted_names=strsplit(names(fitted_parameters),'_')
   for(p in 1:length(fitted_names)) {
@@ -175,13 +175,13 @@ fit_single_scan <- function(fit, data) {
     column_name=paste0(var,"_",model)
     fit$result[sn+1,column_name]=value
   }
-  
+
   #std. error = square root of r^2 divided by number of degrees of freedom (I think???)
   sde=sqrt(sum(residuals(best_fit)^2)/summary(best_fit)$df[2])
-  
+
   #Add to results table
   fit$result[sn+1,"std.error"]=sde
-  
+
   return(fit)
 }
 
@@ -203,17 +203,17 @@ fit_models_to_x_y <- function(models, x, y, control,brute_force=FALSE,brute_forc
   start=list()
   upper=c()
   lower=c()
-  
+
   #Define tolerances to be v.low (not sure whether this helps or not)
   control$ftol=1e-10
   control$ptol=1e-10
   control$etol=1e-10
-  
+
   nModel=length(models)
   for(m in 1:nModel) {
     model=models[[m]]
     #Generate fit formula BEWARE: model numbers here will NOT be the same as in the fit object
-    variables=names(model$constraint) 
+    variables=names(model$constraint)
     formula=paste(formula,paste0("models[[",m,"]]$model(x,",paste(variables,variables,sep='=',collapse=paste0('_',m,',')),'_',m,")"),sep='+')
     #Determine upper and lower values based on constraints
     upper[paste0(names(model$upper),'_',m)]=c(unlist(model$upper))
@@ -222,14 +222,14 @@ fit_models_to_x_y <- function(models, x, y, control,brute_force=FALSE,brute_forc
     start[paste0(names(model$current_guess),'_',m)]=c(unlist(model$current_guess))
   }
   formula=eval(parse(text=formula))
-  
+
   if(brute_force) {
     bf_res=fit_initial_brute_force(models,x,y,formula,upper,lower,grid_points=brute_force_grid_size)
     if(!any(is.na(bf_res))) {
       start=bf_res
     }
   }
-  
+
   #Run the fit
   return(tryCatch({nlsLM(formula, start=start, lower=lower, upper=upper, control = control)}, error = function(e) {
     #warning(e)
@@ -263,7 +263,7 @@ fit_initial_brute_force <-function(models,x,y,formula,upper,lower,grid_points=10
   if(any(is.na(fit))) {
     return(NA)
   }
-  
+
   #Return the best values
   return(as.list(coef(fit)))
 }
@@ -287,18 +287,18 @@ integrate_last_scan <-function(fit,data) {
 
   #Get fitted models
   total_fit=make_fitted_models(models,result_line)
-  
+
   #Build a total fit model
   total_fit_function=get_fit_function(total_fit)
-  
+
   #Integrate total fit
   fit_integral=tryCatch({integrate(total_fit_function,lower=fit$integration_range[[1]],upper=fit$integration_range[[2]])$value}, error = function(e){
     #warning(e)
     0})
   #Store value in results table
   fit$result[scanNo+1,'fit_integrated_area']=fit_integral
-  
-  
+
+
   #Integrate each model independantly
   for(f in total_fit) {
     #Integrate fitted function
@@ -307,9 +307,9 @@ integrate_last_scan <-function(fit,data) {
       0})
     #Store value in results table
     fit$result[scanNo+1,paste0('integrated_area_',f[[1]])]=model_integral
-  
+
     #Find std error for each model, based upon their contribution to the overall model:
-    
+
     #Calculate weighted residuals
     weighted_resids=(f[[2]](x)/total_fit_function(x))*(y-total_fit_function(x))
     weighted_resids=weighted_resids[!is.na(weighted_resids)]
