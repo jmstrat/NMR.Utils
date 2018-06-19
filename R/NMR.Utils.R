cb4392dcb9bcfc22219dd3f041ba56d22983f8a9
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-06-19 01:14:51 +0100

    Use new package exports

--- R/NMR.Utils.R
+++ R/NMR.Utils.R
@@ -4,8 +4,8 @@
 NULL
 
 .onLoad <- function(libname, pkgname){
-  Plotting.Utils::create_data_type('nmr','ppm','',envir=asNamespace('NMR.Utils'))
-  Plotting.Utils::create_data_type('nmr2d','ppm','',inherits='nmr',envir=asNamespace('NMR.Utils'))
-  Plotting.Utils::create_data_type('nmr2dinsitu','ppm','',inherits=c('nmr2d','nmr'),envir=asNamespace('NMR.Utils'))
+  jms.classes::create_data_type('nmr','ppm','',envir=asNamespace('NMR.Utils'))
+  jms.classes::create_data_type('nmr2d','ppm','',inherits='nmr',envir=asNamespace('NMR.Utils'))
+  jms.classes::create_data_type('nmr2dinsitu','ppm','',inherits=c('nmr2d','nmr'),envir=asNamespace('NMR.Utils'))
 }
 
