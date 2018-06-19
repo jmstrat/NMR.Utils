e758e5dbfe806127e9c528b27a839ea231836ed1
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-06-19 01:11:51 +0100

    Export + minor fixes

--- R/Interactive_phase.R
+++ R/Interactive_phase.R
@@ -1,19 +1,26 @@
 
+#' @export
 interactivePhase <- function(data,xrange) {
-  load_or_install('shiny')
-  shinyApp(
-  ui = fluidPage(
-    sidebarLayout(
-      sidebarPanel(sliderInput("scan", "scan", 1,ncol(data)-1,1),sliderInput("p0", "p0", -1000, 1000, 0),sliderInput("p1", "p1", -1000, 1000, 0),sliderInput("pivot", "pivot", xrange[[2]], xrange[[1]], mean(xrange)),sliderInput("yzoom", "yzoom", 1,10,1,step=0.1)),
-      mainPanel(plotOutput("spectrum"))
+  if(missing(xrange)) xrange=rev(range(data[,1]))
+  shiny::shinyApp(
+  ui = shiny::fluidPage(
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(shiny::sliderInput("scan", "scan", 1,ncol(data)-1,1),
+                          shiny::sliderInput("p0", "p0", -1000, 1000, 0),
+                          shiny::sliderInput("p1", "p1", -1000, 1000, 0),
+                          shiny::sliderInput("pivot", "pivot", xrange[[2]], xrange[[1]], mean(xrange)),
+                          shiny::sliderInput("yzoom", "yzoom", 1, 10, 1,step=0.1)),
+      shiny::mainPanel(shiny::plotOutput("spectrum"))
     )
-  ), 
+  ),
   server = function(input, output) {
-    output$spectrum <- renderPlot({ 
+    output$spectrum <- shiny::renderPlot({
       phased=phase(data[,c(1,input$scan+1)],input$p0,input$p1,input$pivot)
       phased=makeReal(phased)
       yr=c(min(phased[,2]),max(phased[,2]/input$yzoom))
-      plot(phased,type='l',xlim=xrange,xaxs='i',yaxs='i',yaxt='n',ylab='Intensity',ylim=yr) }
+      plot(phased,type='l',xlim=xrange,xaxs='i',yaxs='i',yaxt='n',ylab='Intensity',ylim=yr)
+      abline(v=input$pivot, col='red')
+      }
     )
   }
 )
