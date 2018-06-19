a7a1e3fd2071aa420c489a3a9f7603176354ec90
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-06-19 01:13:21 +0100

    Rexports

---
+++ R/zzz_jms.reexports.R
@@ -0,0 +1,76 @@
+##' Reexported functions from jms.classes
+##' @name reexports
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes iplot
+##' @name iplot
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes export
+##' @name export
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes xlab
+##' @name xlab
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes ylab
+##' @name ylab
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes y2lab
+##' @name y2lab
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+
+##' @importFrom jms.classes xcol
+##' @name xcol
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes ycol
+##' @name ycol
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes y2col
+##' @name y2col
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes make_background
+##' @name make_background
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes undersample
+##' @name undersample
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
