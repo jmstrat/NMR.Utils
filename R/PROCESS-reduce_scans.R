6c416bd07118c45c8e2eda48f6e59b1a26dd9d18
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-06-24 16:01:56 +0100

    Add find_last_scan

--- R/PROCESS-reduce_scans.R
+++ R/PROCESS-reduce_scans.R
@@ -16,3 +16,17 @@ reduceScans <-function(spectra,scan_range) {
     scan_range[[2]]=n
   spectra[,c(1,scan_range[[1]]:scan_range[[2]])]
 }
+
+#' Finds the number of the last (non-empty) scan
+#'
+#' @param spectra A dataframe of NMR data
+#' @param tol If all the intensities are equal to within this tolerance the scan is considered empty
+#' @return The number of the last scan
+#' @export
+#' @examples
+#' find_last_scan(spectra)
+find_last_scan <- function(spectra, tol=0.01) {
+  not_missing_scans <- which(!sapply(spectra, function(y) abs(max(y) - min(y)) < tol))
+  if(length(not_missing_scans)) return(not_missing_scans[[length(not_missing_scans)]]-1)
+  0
+}
