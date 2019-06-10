5f199aec72d5b8c19a7ad146f974123332bb0469
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 15:23:56 +0100

    Improve export for insitu data

---
+++ R/DATA-export.R
@@ -0,0 +1,11 @@
+#' Export insitu NMR data
+#'
+#' @export
+export.nmr2dinsitu.data.object <- function(data, path) {
+  echem = attr(data, 'echem')
+  attr(data, 'echem') <- NULL
+  class(data) <- class(data)[-1]
+  export(data, path)
+  echem_path = paste0(tools::file_path_sans_ext(path), '-echem-data.csv')
+  export(echem, echem_path)
+}
