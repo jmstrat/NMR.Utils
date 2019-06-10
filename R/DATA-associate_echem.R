f26b22cbe1e6da973dbe9b2f7ed496775d8d8acf
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 15:24:24 +0100

    Verify echem

--- R/DATA-associate_echem.R
+++ R/DATA-associate_echem.R
@@ -5,8 +5,28 @@
 #' @return An insitu.nmr.2Ddata.object
 #' @export
 associate_echem_with_nmr <- function(nmr,echem) {
+  if(!requireNamespace("Echem.Data", quietly=TRUE)) {
+    stop('Insitu data processing requires the Echem.Data package')
+  }
   #Check validity of echem
   if(!Echem.Data::is.echem.data.object(echem)) stop('Echem object must be of class echem.data.object')
+  if(length(xcol(echem)) != 1 || is.na(xcol(echem))) {
+    jms.classes::log.error(
+      'xcol is not correct: xcol = [%s]; ycol = [%s]',
+      paste0(xcol(echem), collapse=','),
+      paste0(ycol(echem), collapse=','))
+    stop('Invalid echem data')
+  }
+
+  if(length(ycol(echem)) != 1 || is.na(ycol(echem))) {
+    jms.classes::log.error(
+      'ycol is not correct: xcol = [%s]; ycol = [%s]',
+      paste0(xcol(echem), collapse=','),
+      paste0(ycol(echem), collapse=','))
+    stop('Invalid echem data')
+  }
+
+  jms.classes::log.info('Combining echem and NMR objects')
   nmr=as.nmr2dinsitu.data.object(nmr)
   attr(nmr, "echem")<- echem
   return(nmr)
