b5b1b39e02af5070c41055f463aa4620c17d93c9
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 15:17:25 +0100

    More reexports

--- R/zzz_jms.reexports.R
+++ R/zzz_jms.reexports.R
@@ -39,6 +39,26 @@ NULL
 ##' @keywords internal
 NULL
 
+##' @importFrom jms.classes xlab<-
+##' @name xlab<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes ylab<-
+##' @name ylab<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes y2lab<-
+##' @name y2lab<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
 
 ##' @importFrom jms.classes xcol
 ##' @name xcol
@@ -61,8 +81,78 @@ NULL
 ##' @keywords internal
 NULL
 
+##' @importFrom jms.classes xcol<-
+##' @name xcol<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes ycol<-
+##' @name ycol<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes y2col<-
+##' @name y2col<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes xscale
+##' @name xscale
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes yscale
+##' @name yscale
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes y2scale
+##' @name y2scale
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes xscale<-
+##' @name xscale<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes yscale<-
+##' @name yscale<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes y2scale<-
+##' @name y2scale<-
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
 ##' @importFrom jms.classes make_background
 ##' @name make_background
+##' @export
+##' @rdname jms.reexports
+##' @keywords internal
+NULL
+
+##' @importFrom jms.classes expand_baseline_parameters
+##' @name expand_baseline_parameters
 ##' @export
 ##' @rdname jms.reexports
 ##' @keywords internal
