c6a235dcdf2aeeb1bcfda64b6c4d73785960400a
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 16:49:34 +0100

    lint

--- R/DATA-export.R
+++ R/DATA-export.R
@@ -2,10 +2,10 @@
 #'
 #' @export
 export.nmr2dinsitu.data.object <- function(data, path) {
-  echem = attr(data, 'echem')
-  attr(data, 'echem') <- NULL
+  echem <- attr(data, "echem")
+  attr(data, "echem") <- NULL
   class(data) <- class(data)[-1]
   export(data, path)
-  echem_path = paste0(tools::file_path_sans_ext(path), '-echem-data.csv')
+  echem_path <- paste0(tools::file_path_sans_ext(path), "-echem-data.csv")
   export(echem, echem_path)
 }
