ab57e24984416bd5dea1a39798f7475510cf2539
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 16:52:03 +0100

    lint

--- R/FIT-curves.R
+++ R/FIT-curves.R
@@ -9,11 +9,13 @@
 #' @return The value(s) of the pseudo voigt function evaluated with the specified parameters
 #' @export
 #' @examples
-#' pseudoVoigt(10,10,0,2,0.5)
-#' pseudoVoigt(c(1:10),10,0,2,0.5)
+#' pseudoVoigt(10, 10, 0, 2, 0.5)
+#' pseudoVoigt(c(1:10), 10, 0, 2, 0.5)
 #'
-#' f=function(x) {pseudoVoigt(x,height=10,centre=0,hwhm=2,shape=0.5)}
-#' curve(f,from=-5,to=5)
-pseudoVoigt <- function(x,height,centre,hwhm,shape) {
-  height*((1-shape)*exp(-log(2)*((x-centre)/hwhm)^2)+shape/(1+((x-centre)/hwhm)^2))
+#' f <- function(x) {
+#'   pseudoVoigt(x, height=10, centre=0, hwhm=2, shape=0.5)
+#' }
+#' curve(f, from=-5, to=5)
+pseudoVoigt <- function(x, height, centre, hwhm, shape) {
+  height * ((1 - shape) * exp(-log(2) * ((x - centre) / hwhm)^2) + shape / (1 + ((x - centre) / hwhm)^2))
 }
