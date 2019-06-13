92b8616b8db496d0df18baf38b9aa4a02db4621b
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 19:33:06 +0100

    lint

--- R/IMPORT-nmr.text.1d.R
+++ R/IMPORT-nmr.text.1d.R
@@ -7,16 +7,16 @@
 #' read.nmr.text.1d.1col("/path/to/file.txt")
 #' @keywords internal
 read.nmr.text.1d.1col <- function(nmrfile) {
-  jms.classes::log.info('Reading 1D 1 column file: %s', nmrfile)
-  ppmscale=read.nmr.text.1d.1col.ppm(nmrfile)
+  jms.classes::log.info("Reading 1D 1 column file: %s", nmrfile)
+  ppmscale <- read.nmr.text.1d.1col.ppm(nmrfile)
 
-  #Read all data ignoring comment lines (Thus also ignoring row separators)
-  fullTable=read.table(nmrfile)
+  # Read all data ignoring comment lines (Thus also ignoring row separators)
+  fullTable <- read.table(nmrfile)
 
-  #Make data frame
-  nmrdata=nmr.data.object(ppmscale,fullTable$V1)
-  #Rename columns
-  names(nmrdata) <- c("ppm","intensity")
+  # Make data frame
+  nmrdata <- nmr.data.object(ppmscale, fullTable$V1)
+  # Rename columns
+  names(nmrdata) <- c("ppm", "intensity")
   return(nmrdata)
 }
 
@@ -28,15 +28,15 @@ read.nmr.text.1d.1col <- function(nmrfile) {
 #' @examples
 #' read.nmr.text.1d.2col("/path/to/file.txt")
 #' @keywords internal
-read.nmr.text.1d.2col <- function(file, skip, sep = "") {
-  jms.classes::log.info('Reading 1D 2 column file: %s', file)
-  tab = read.table.nmr(file,skip=skip, sep = sep)
-  n = ncol(tab)
-  if(n == 2) {
-    names(tab) <- c("ppm","intensity")
+read.nmr.text.1d.2col <- function(file, skip, sep="") {
+  jms.classes::log.info("Reading 1D 2 column file: %s", file)
+  tab <- read.table.nmr(file, skip=skip, sep=sep)
+  n <- ncol(tab)
+  if (n == 2) {
+    names(tab) <- c("ppm", "intensity")
   } else {
     # ascii-spec
-    names(tab) <- c("point","intensity","hz","ppm")
+    names(tab) <- c("point", "intensity", "hz", "ppm")
   }
   tab
 }
@@ -50,16 +50,16 @@ read.nmr.text.1d.2col <- function(file, skip, sep = "") {
 #' read.nmr.text.1d("/path/to/file.txt")
 #' @keywords internal
 read.nmr.text.1d <- function(file) {
-  type_skip=determine_1d_file_type(file)
-  if(type_skip[[1]]==1) {
-    data = read.nmr.text.1d.1col(file)
-  } else if(type_skip[[1]]==2) {
-    data = read.nmr.text.1d.2col(file,type_skip[[2]],type_skip[[3]])
+  type_skip <- determine_1d_file_type(file)
+  if (type_skip[[1]] == 1) {
+    data <- read.nmr.text.1d.1col(file)
+  } else if (type_skip[[1]] == 2) {
+    data <- read.nmr.text.1d.2col(file, type_skip[[2]], type_skip[[3]])
   } else {
     stop("File type not supported")
   }
-  jms.classes::xcol(data) <- 'ppm'
-  jms.classes::ycol(data) <- 'intensity'
+  jms.classes::xcol(data) <- "ppm"
+  jms.classes::ycol(data) <- "intensity"
   data
 }
 
@@ -71,24 +71,24 @@ read.nmr.text.1d <- function(file) {
 #' @examples
 #' read.nmr.text.1d.1col.ppm("/path/to/file.txt")
 #' @keywords internal
-read.nmr.text.1d.1col.ppm <-function(file) {
-  #Extract info from preamble
-  #Open file for reading
-  con=file(file,open='r')
-  #Read first 8 lines
-  lines=readLines(con,n=8)
-  #Close file
+read.nmr.text.1d.1col.ppm <- function(file) {
+  # Extract info from preamble
+  # Open file for reading
+  con <- file(file, open="r")
+  # Read first 8 lines
+  lines <- readLines(con, n=8)
+  # Close file
   close(con)
-  #Extract lines
-  lr=lines[[4]]
-  nr=lines[[6]]
+  # Extract lines
+  lr <- lines[[4]]
+  nr <- lines[[6]]
 
-  #Get data from lines
-  l=as.numeric(gsub("# LEFT = ([[:digit:]\\.-]*) .*","\\1",lr))
-  r=as.numeric(gsub("# LEFT = [[:digit:]\\.-]* ppm\\. RIGHT = ([[:digit:]\\.-]*) .*","\\1",lr))
-  nrows=as.numeric(gsub("# SIZE = ([[:digit:]]*).*","\\1",nr))
+  # Get data from lines
+  l <- as.numeric(gsub("# LEFT = ([[:digit:]\\.-]*) .*", "\\1", lr))
+  r <- as.numeric(gsub("# LEFT = [[:digit:]\\.-]* ppm\\. RIGHT = ([[:digit:]\\.-]*) .*", "\\1", lr))
+  nrows <- as.numeric(gsub("# SIZE = ([[:digit:]]*).*", "\\1", nr))
 
-  #Calculate ppm scale
-  ppmscale=seq(l,r,(r-l)/(nrows-1))
+  # Calculate ppm scale
+  ppmscale <- seq(l, r, (r - l) / (nrows - 1))
   return(ppmscale)
 }
