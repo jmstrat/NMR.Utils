46afda29c52f4cef4673a5dd1a00de9de6dfdade
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 18:21:15 +0100

    lint

--- R/GUI-PLOTTING-ANNOTATION-LABEL.R
+++ R/GUI-PLOTTING-ANNOTATION-LABEL.R
@@ -10,19 +10,18 @@ label_annotation_mod_UI <- function(id) {
   ns <- shiny::NS(id)
   shiny::tagList(
     shiny::fluidRow(
-      shiny::column(3, shiny::textInput(ns('label'), 'Label', 'Label')),
-      shiny::column(3, shiny::selectInput(ns('pos'), 'Position', names(label_posns))),
-      shiny::column(2, shiny::numericInput(ns('cex'), 'Relative Size', 1, min=0, step=0.5)),
-      shiny::column(2, restorableColourInput(ns('col'), 'Colour', value='black'))
+      shiny::column(3, shiny::textInput(ns("label"), "Label", "Label")),
+      shiny::column(3, shiny::selectInput(ns("pos"), "Position", names(label_posns))),
+      shiny::column(2, shiny::numericInput(ns("cex"), "Relative Size", 1, min=0, step=0.5)),
+      shiny::column(2, restorableColourInput(ns("col"), "Colour", value="black"))
     )
   )
 }
 
 label_annotation_mod <- function(input, output, session) {
-
   arguments <- function() {
-    pos=input$pos
-    if(is.null(pos)) pos='Centred'
+    pos <- input$pos
+    if (is.null(pos)) pos <- "Centred"
     return(list(
       label=input$label,
       pos=label_posns[[pos]],
@@ -32,36 +31,36 @@ label_annotation_mod <- function(input, output, session) {
   }
 
   updateInputs <- function(label, pos, cex, col, xpd=NULL) {
-    shiny::updateTextInput(session, 'label', value=label)
+    shiny::updateTextInput(session, "label", value=label)
 
-    if(is.null(pos)) {
-      pos='Centred'
+    if (is.null(pos)) {
+      pos <- "Centred"
     } else {
-      options = unlist(label_posns)
-      pos = names(options)[options == pos]
+      options <- unlist(label_posns)
+      pos <- names(options)[options == pos]
     }
 
-    shiny::updateSelectInput(session, 'pos', selected=pos)
-    shiny::updateNumericInput(session, 'cex', value=cex)
-    colourpicker::updateColourInput(session, 'col', value=col)
+    shiny::updateSelectInput(session, "pos", selected=pos)
+    shiny::updateNumericInput(session, "cex", value=cex)
+    colourpicker::updateColourInput(session, "col", value=col)
     return()
   }
 
   getBoundingRect <- function(x, y, label, pos, cex, col, xpd=NULL) {
-    offset = 0.5 * par("cxy")
+    offset <- 0.5 * par("cxy")
 
-    if(is.null(pos)) {
+    if (is.null(pos)) {
       adj <- c(0.5, 0.5)
-    } else if(pos == 1) {
+    } else if (pos == 1) {
       adj <- c(0.5, 1)
       y <- y - offset[[2]]
-    } else if(pos == 2) {
+    } else if (pos == 2) {
       adj <- c(1, 0.5)
       x <- x - offset[[1]]
-    } else if(pos == 3) {
+    } else if (pos == 3) {
       adj <- c(0.5, 0)
       y <- y + offset[[2]]
-    } else if(pos == 4) {
+    } else if (pos == 4) {
       adj <- c(0, 0.5)
       x <- x + offset[[1]]
     }
@@ -93,19 +92,19 @@ label_annotation_mod <- function(input, output, session) {
       bounds[[2]] - yinch() * 0.1,
       bounds[[3]] + xinch() * 0.1,
       bounds[[4]] + yinch() * 0.1,
-      border = 'red',
-      lwd = 2
+      border="red",
+      lwd=2
     )
   }
 
   script <- function(x, y, label, pos, cex, col, xpd=NULL) {
-    xpds = ''
-    if(!is.null(xpd)) {
-      xpds = sprintf(', xpd=%s', xpd)
+    xpds <- ""
+    if (!is.null(xpd)) {
+      xpds <- sprintf(", xpd=%s", xpd)
     }
-    poss = ''
-    if(!is.null(pos)) {
-      poss = sprintf(', pos=%s', pos)
+    poss <- ""
+    if (!is.null(pos)) {
+      poss <- sprintf(", pos=%s", pos)
     }
     sprintf(
       'text(%s, %s, "%s"%s, cex=%s, col="%s"%s)',
@@ -123,4 +122,4 @@ label_annotation_mod <- function(input, output, session) {
   ))
 }
 
-register_plot_annotation_module('Label', label_annotation_mod_UI, label_annotation_mod)
+register_plot_annotation_module("Label", label_annotation_mod_UI, label_annotation_mod)
