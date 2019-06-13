7052e7567e300c27ec0760abb3e73d88cd4007ae
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 17:44:09 +0100

    lint

--- R/GUI-EXPORT.R
+++ R/GUI-EXPORT.R
@@ -1,5 +1,5 @@
 export_UI <- function(id) {
-  ns = shiny::NS(id)
+  ns <- shiny::NS(id)
   shiny::tagList(
     shiny::h3("Processing scripts"),
     shiny::fluidRow(
@@ -11,9 +11,9 @@ export_UI <- function(id) {
     shiny::fluidRow(
       shiny::column(
         12,
-        shiny::uiOutput(ns('rstudioUI'), inline=TRUE),
-        shiny::actionButton(ns("copy_script"), "Copy processing script", style='margin-top:20px'),
-        shiny::downloadButton(ns('save_script'), 'Save processing script', style='margin-top:20px')
+        shiny::uiOutput(ns("rstudioUI"), inline=TRUE),
+        shiny::actionButton(ns("copy_script"), "Copy processing script", style="margin-top:20px"),
+        shiny::downloadButton(ns("save_script"), "Save processing script", style="margin-top:20px")
       )
     )
   )
@@ -25,27 +25,32 @@ export_server <- function(input, output, session, data, data_name, data_filename
   })
 
   output$save_script <- shiny::downloadHandler(
-    filename = function() {paste0(tools::file_path_sans_ext(data_filename()), '-nmr-processing.R')},
-    content = function(file) {writeLines(script(), file)})
+    filename=function() {
+      paste0(tools::file_path_sans_ext(data_filename()), "-nmr-processing.R")
+    },
+    content=function(file) {
+      writeLines(script(), file)
+    }
+  )
 
   output$rstudioUI <- shiny::renderUI({
     # documentNew was introduced in version 1.1.640
-    if(rstudioapi::isAvailable('1.1.640')) {
-      shiny::actionButton(session$ns('openRstudio'), 'Open script in RStudio', style='background: #77dd77; margin-top:20px;')
+    if (rstudioapi::isAvailable("1.1.640")) {
+      shiny::actionButton(session$ns("openRstudio"), "Open script in RStudio", style="background: #77dd77; margin-top:20px;")
     }
   })
 
   shiny::observeEvent(input$openRstudio, {
     gui_try_show_error({
       rstudioapi::documentNew(script())
-    }, 'making a new document')
+    }, "making a new document")
   })
 
   export_script <- shiny::reactive({
     sprintf(
       "# Processed Data can be exported using the following command:\n# export(%s, '%s')\n\n# See ?NMR.Utils for details on other methods to process the data (including fitting and animations)",
       data_name(),
-      paste0(tools::file_path_sans_ext(data_filename()), '-processed-data.csv')
+      paste0(tools::file_path_sans_ext(data_filename()), "-processed-data.csv")
     )
   })
 
@@ -53,6 +58,6 @@ export_server <- function(input, output, session, data, data_name, data_filename
   shiny::setBookmarkExclude(c("copy_script", "save_script", "openRstudio"))
 
   list(
-    script = export_script
+    script=export_script
   )
 }
