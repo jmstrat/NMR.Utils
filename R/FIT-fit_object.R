658d3e4eeaf1ad052fcfe81cb8fb239ea1736f2a
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 16:54:53 +0100

    lint

--- R/FIT-fit_object.R
+++ R/FIT-fit_object.R
@@ -13,9 +13,19 @@
 #' @export
 #' @examples
 #' new_fit()
-new_fit <- function(integration_range=c(-Inf,Inf),noise_height=0,maxEvaluations=10000,maxIterations=1000,use_previous_as_guess=TRUE, brute_force=FALSE,brute_force_method='short',brute_force_grid_size=10) {
-  fit = list(models=list(),integration_range=integration_range,noise_height=noise_height, maxEvaluations=maxEvaluations, maxIterations=maxIterations, determine_parameters_dynamically=FALSE, determine_parameters_from_scans=NA, use_previous_as_guess=use_previous_as_guess,brute_force=brute_force, brute_force_method=brute_force_method,brute_force_grid_size=brute_force_grid_size,update_guess_for_non_fixed=TRUE,ppm_exclude=list(),result=NA,last_scan=0, timeAxis=NA)
-  class(fit) <- c('nmr.fit.object', 'list')
+new_fit <- function(integration_range=c(-Inf, Inf), noise_height=0, maxEvaluations=10000,
+                    maxIterations=1000, use_previous_as_guess=TRUE, brute_force=FALSE,
+                    brute_force_method="short", brute_force_grid_size=10) {
+  fit <- list(
+    models=list(), integration_range=integration_range, noise_height=noise_height,
+    maxEvaluations=maxEvaluations, maxIterations=maxIterations,
+    determine_parameters_dynamically=FALSE, determine_parameters_from_scans=NA,
+    use_previous_as_guess=use_previous_as_guess, brute_force=brute_force,
+    brute_force_method=brute_force_method, brute_force_grid_size=brute_force_grid_size,
+    update_guess_for_non_fixed=TRUE, ppm_exclude=list(), result=NA, last_scan=0,
+    timeAxis=NA
+  )
+  class(fit) <- c("nmr.fit.object", "list")
   fit
 }
 
@@ -27,9 +37,9 @@ new_fit <- function(integration_range=c(-Inf,Inf),noise_height=0,maxEvaluations=
 #' @return None
 #' @export
 #' @examples
-#' export(fit,"/path/to/file.csv")
+#' export(fit, "/path/to/file.csv")
 export.nmr.fit.object <- function(fit, filename) {
-  write.table(fit$result,filename,sep=",",row.names =FALSE)
+  write.table(fit$result, filename, sep=",", row.names=FALSE)
 }
 
 
@@ -41,10 +51,10 @@ export.nmr.fit.object <- function(fit, filename) {
 #' @param update_guess_for_non_fixed Use the automatically determined parameters for non-fixed parameters (as well as for the fixed ones)
 #' @return The fit object
 #' @export
-make_dynamic_fit <-function(fit,scans, update_guess_for_non_fixed=TRUE) {
-  fit$determine_parameters_dynamically=TRUE
-  fit$determine_parameters_from_scans=scans
-  fit$update_guess_for_non_fixed=update_guess_for_non_fixed
+make_dynamic_fit <- function(fit, scans, update_guess_for_non_fixed=TRUE) {
+  fit$determine_parameters_dynamically <- TRUE
+  fit$determine_parameters_from_scans <- scans
+  fit$update_guess_for_non_fixed <- update_guess_for_non_fixed
   return(fit)
 }
 
@@ -55,8 +65,8 @@ make_dynamic_fit <-function(fit,scans, update_guess_for_non_fixed=TRUE) {
 #' @param range The range to exclude c(start,end) -- start < end
 #' @return The fit object
 #' @export
-exclude_ppm_range_from_fit <-function(fit,range) {
-  fit$ppm_exclude=append(fit$ppm_exclude, range)
+exclude_ppm_range_from_fit <- function(fit, range) {
+  fit$ppm_exclude <- append(fit$ppm_exclude, range)
   return(fit)
 }
 
@@ -68,11 +78,12 @@ exclude_ppm_range_from_fit <-function(fit,range) {
 #' @return A fit object
 #' @export
 #' @examples
-#' add_model_to_fit(fit,model)
-add_model_to_fit <- function(fit,model) {
-  if(is.na(model$name))
-    model$name = deparse(substitute(model))
-  fit$models=append(fit$models,list(model))
+#' add_model_to_fit(fit, model)
+add_model_to_fit <- function(fit, model) {
+  if (is.na(model$name)) {
+    model$name <- deparse(substitute(model))
+  }
+  fit$models <- append(fit$models, list(model))
   return(fit)
 }
 
@@ -84,16 +95,16 @@ add_model_to_fit <- function(fit,model) {
 #' @examples
 #' fix_unconstrained_parameters(fit)
 #' @keywords internal
-fix_unconstrained_parameters <-function(fit) {
-  lenm=length(fit$models)
-  for(m in 1:lenm) {
-    model=fit$models[[m]]
-    fmls=names(formals(model$model))
-    fixed=fmls[!fmls %in% names(model$constraint)]
-    fixed=fixed[!fixed=='x']
-    formals(model$model)[fixed]=model$initial_guess[fixed]
-    model$current_guess=model$initial_guess[names(model$constraint)]
-    fit$models[[m]]=model
+fix_unconstrained_parameters <- function(fit) {
+  lenm <- length(fit$models)
+  for (m in 1:lenm) {
+    model <- fit$models[[m]]
+    fmls <- names(formals(model$model))
+    fixed <- fmls[!fmls %in% names(model$constraint)]
+    fixed <- fixed[!fixed == "x"]
+    formals(model$model)[fixed] <- model$initial_guess[fixed]
+    model$current_guess <- model$initial_guess[names(model$constraint)]
+    fit$models[[m]] <- model
   }
   return(fit)
 }
@@ -105,35 +116,35 @@ fix_unconstrained_parameters <-function(fit) {
 #' @param n The number of rows to add to the data frame
 #' @return The fit object
 #' @examples
-#' prepare_result_data_frame(fit,n)
+#' prepare_result_data_frame(fit, n)
 #' @keywords internal
-prepare_result_data_frame <-function(fit,n) {
-  columns=c("std.error",'fit_integrated_area')
-  model_extra_columns=c('std.error','integrated_area')
-  lenm=length(fit$models)
-  for(m in 1:lenm) {
-    model=fit$models[[m]]
-    columns=append(columns, paste0(names(model$constraint),'_',m))
-    columns=append(columns, paste0(model_extra_columns,'_',m))
+prepare_result_data_frame <- function(fit, n) {
+  columns <- c("std.error", "fit_integrated_area")
+  model_extra_columns <- c("std.error", "integrated_area")
+  lenm <- length(fit$models)
+  for (m in 1:lenm) {
+    model <- fit$models[[m]]
+    columns <- append(columns, paste0(names(model$constraint), "_", m))
+    columns <- append(columns, paste0(model_extra_columns, "_", m))
   }
-  fit$result=data.frame(rep_len(list(rep_len(NA,n)),length(columns)))
-  names(fit$result)<-columns
+  fit$result <- data.frame(rep_len(list(rep_len(NA, n)), length(columns)))
+  names(fit$result) <- columns
   return(fit)
 }
 
-#'Generate fit function
+#' Generate fit function
 #'
 #' This function creates a function representing the overall fit
 #' @param models List of models to use
 #' @return The fit function
 #' @examples
-#' func=get_fit_function(models)
+#' func <- get_fit_function(models)
 #' @keywords internal
 get_fit_function <- function(models) {
-  if(length(models)>0) {
-    total_fit_function=function(x) rowSums(sapply(models,function(y) y[[2]](x)))
+  if (length(models) > 0) {
+    total_fit_function <- function(x) rowSums(sapply(models, function(y) y[[2]](x)))
   } else {
-    total_fit_function=function(x) rep_len(0,length(x))
+    total_fit_function <- function(x) rep_len(0, length(x))
   }
   return(total_fit_function)
 }
@@ -145,34 +156,34 @@ get_fit_function <- function(models) {
 #' @param result_line A row from the results table
 #' @return list(c(model_index, fitted_model),...)
 #' @examples
-#' make_fitted_models(models,result_line)
+#' make_fitted_models(models, result_line)
 #' @keywords internal
-make_fitted_models <-function(models,result_line) {
-  nmodels=length(models)
-  total_fit=c()
-  for(m in 1:nmodels) {
-    model=models[[m]]
+make_fitted_models <- function(models, result_line) {
+  nmodels <- length(models)
+  total_fit <- c()
+  for (m in 1:nmodels) {
+    model <- models[[m]]
 
-    #Extract fitted values
-    variables=names(model$constraint)
-    column_names=paste0(variables,"_",m)
-    values=as.list(result_line[,column_names])
-    names(values)<-variables
-    if(any(is.na(values))) {
+    # Extract fitted values
+    variables <- names(model$constraint)
+    column_names <- paste0(variables, "_", m)
+    values <- as.list(result_line[, column_names])
+    names(values) <- variables
+    if (any(is.na(values))) {
       next
     } else {
 
-      #Define fitted function
-      model_fitted=model$model
+      # Define fitted function
+      model_fitted <- model$model
 
-      #Assign fitted values to it
-      fmls=names(formals(model_fitted))
-      fixed=fmls[fmls %in% variables]
-      formals(model_fitted)[fixed]=values[fixed]
+      # Assign fitted values to it
+      fmls <- names(formals(model_fitted))
+      fixed <- fmls[fmls %in% variables]
+      formals(model_fitted)[fixed] <- values[fixed]
 
-      #Add function to overall fit
-      total_fit=append(total_fit,list(c(m,model_fitted)))
+      # Add function to overall fit
+      total_fit <- append(total_fit, list(c(m, model_fitted)))
     }
   }
-    return(total_fit)
+  return(total_fit)
 }
