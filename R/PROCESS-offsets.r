d3e7f34d8b69a7671f082bb8c158dae031f4e47d
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 20:14:37 +0100

    lint

--- R/PROCESS-offsets.r
+++ R/PROCESS-offsets.r
@@ -6,10 +6,10 @@
 #' @return A vector of offsets
 #' @export
 #' @examples
-#' noATMoffsets(spectra,0.5)
-noATMoffsets <-function(spectra,scan_length,initial_offset=0) {
-  n=ncol(spectra)-1
-  return(0:n*scan_length+initial_offset)
+#' noATMoffsets(spectra, 0.5)
+noATMoffsets <- function(spectra, scan_length, initial_offset=0) {
+  n <- ncol(spectra) - 1
+  return(0:n * scan_length + initial_offset)
 }
 
 #' Stores the time offsets in the data frame
@@ -19,16 +19,16 @@ noATMoffsets <-function(spectra,scan_length,initial_offset=0) {
 #' @return An idential data frame whose names correspond to the offsets
 #' @export
 #' @examples
-#' storeOffsets(data,offsets)
-storeOffsets <-function(spectra,offsets) {
-  jms.classes::log.info('Adding scan offsets to 2D NMR data')
-  lenO=length(offsets)
-  lenS=ncol(spectra)-1
-  if(lenO<lenS) {
-    offsets=append(offsets,(lenO+1):lenS)
-  } else if(lenS<lenO) {
-    offsets=offsets[c(1:lenS)]
+#' storeOffsets(data, offsets)
+storeOffsets <- function(spectra, offsets) {
+  jms.classes::log.info("Adding scan offsets to 2D NMR data")
+  lenO <- length(offsets)
+  lenS <- ncol(spectra) - 1
+  if (lenO < lenS) {
+    offsets <- append(offsets, (lenO + 1):lenS)
+  } else if (lenS < lenO) {
+    offsets <- offsets[c(1:lenS)]
   }
-  names(spectra)[c(2:(lenS+1))]<-offsets
+  names(spectra)[c(2:(lenS + 1))] <- offsets
   return(spectra)
 }
