d4e3246e6b6115ca757113dbd19c76ed4fd2bfa1
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 16:49:07 +0100

    lint

--- R/DATA-associate_echem.R
+++ R/DATA-associate_echem.R
@@ -4,45 +4,49 @@
 #' @param Echem The echem data
 #' @return An insitu.nmr.2Ddata.object
 #' @export
-associate_echem_with_nmr <- function(nmr,echem) {
-  if(!requireNamespace("Echem.Data", quietly=TRUE)) {
-    stop('Insitu data processing requires the Echem.Data package')
+associate_echem_with_nmr <- function(nmr, echem) {
+  if (!requireNamespace("Echem.Data", quietly=TRUE)) {
+    stop("Insitu data processing requires the Echem.Data package")
   }
-  #Check validity of echem
-  if(!Echem.Data::is.echem.data.object(echem)) stop('Echem object must be of class echem.data.object')
-  if(length(xcol(echem)) != 1 || is.na(xcol(echem))) {
+  # Check validity of echem
+  if (!Echem.Data::is.echem.data.object(echem)) stop("Echem object must be of class echem.data.object")
+  if (length(xcol(echem)) != 1 || is.na(xcol(echem))) {
     jms.classes::log.error(
-      'xcol is not correct: xcol = [%s]; ycol = [%s]',
-      paste0(xcol(echem), collapse=','),
-      paste0(ycol(echem), collapse=','))
-    stop('Invalid echem data')
+      "xcol is not correct: xcol = [%s]; ycol = [%s]",
+      paste0(xcol(echem), collapse=","),
+      paste0(ycol(echem), collapse=",")
+    )
+    stop("Invalid echem data")
   }
 
-  if(length(ycol(echem)) != 1 || is.na(ycol(echem))) {
+  if (length(ycol(echem)) != 1 || is.na(ycol(echem))) {
     jms.classes::log.error(
-      'ycol is not correct: xcol = [%s]; ycol = [%s]',
-      paste0(xcol(echem), collapse=','),
-      paste0(ycol(echem), collapse=','))
-    stop('Invalid echem data')
+      "ycol is not correct: xcol = [%s]; ycol = [%s]",
+      paste0(xcol(echem), collapse=","),
+      paste0(ycol(echem), collapse=",")
+    )
+    stop("Invalid echem data")
   }
 
-  jms.classes::log.info('Combining echem and NMR objects')
-  nmr=as.nmr2dinsitu.data.object(nmr)
-  attr(nmr, "echem")<- echem
-  if(is.null(attr(nmr, ".scantimes"))) attr(nmr, ".scantimes")<- as.numeric(colnames(nmr)[-1])
+  jms.classes::log.info("Combining echem and NMR objects")
+  nmr <- as.nmr2dinsitu.data.object(nmr)
+  attr(nmr, "echem") <- echem
+  if (is.null(attr(nmr, ".scantimes"))) attr(nmr, ".scantimes") <- as.numeric(colnames(nmr)[-1])
   transformOffsetsToXAxis(nmr, echem)
 }
 
 transformOffsetsToXAxis <- function(nmr, echem) {
   # Do we need to transform?
   xc <- jms.classes::xcol(echem)
-  currencXC <- attr(nmr, '.offset_xcol')
-  if(is.null(currencXC)) currencXC = which(colnames(echem) == 'Test_Time.s.')
-  if(currencXC == xc) return(nmr)
-  attr(nmr, '.offset_xcol') <- xc
-  jms.classes::log.info('Adjusting offsets to match %s', colnames(echem)[xc])
+  currencXC <- attr(nmr, ".offset_xcol")
+  if (is.null(currencXC)) currencXC <- which(colnames(echem) == "Test_Time.s.")
+  if (currencXC == xc) {
+    return(nmr)
+  }
+  attr(nmr, ".offset_xcol") <- xc
+  jms.classes::log.info("Adjusting offsets to match %s", colnames(echem)[xc])
   # Transform the offsets
-  offsets = attr(nmr, ".scantimes")
-  new_offsets = Plotting.Utils::nearest(echem, Test_Time.s.=offsets*3600)[,xc] * jms.classes::xscale(echem)
+  offsets <- attr(nmr, ".scantimes")
+  new_offsets <- Plotting.Utils::nearest(echem, Test_Time.s.=offsets * 3600)[, xc] * jms.classes::xscale(echem)
   storeOffsets(nmr, new_offsets)
 }
