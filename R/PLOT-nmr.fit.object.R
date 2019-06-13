6fc7a0ef945d1a61caf75bbcdcfc09e73336e535
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 20:05:14 +0100

    lint

--- R/PLOT-nmr.fit.object.R
+++ R/PLOT-nmr.fit.object.R
@@ -14,65 +14,71 @@
 #' @export
 #' @examples
 #' plot(fit)
-plot.nmr.fit.object <- function(fit,showErrors=TRUE, NAasZero=FALSE, showLegend=TRUE, pch=4, fit_col='red',deconv_cols=c('forestgreen','blue','magenta','cyan','orange','brown'), error_alpha=0.3,plot_total_integral=TRUE) {
-  if(all(is.na(fit$result))) {
-    stop('Fit must be run before plotting!')
+plot.nmr.fit.object <- function(fit, showErrors=TRUE, NAasZero=FALSE, showLegend=TRUE, pch=4,
+                                fit_col="red", deconv_cols=c("forestgreen", "blue", "magenta", "cyan", "orange", "brown"),
+                                error_alpha=0.3, plot_total_integral=TRUE) {
+  if (all(is.na(fit$result))) {
+    stop("Fit must be run before plotting!")
   }
-  if(all(is.na(fit$timeAxis))) {
-    Plotting.Utils::pretty_plot(c(0,1),c(0,100),ylab = "Relative area (%)", xlab = 'Time', axes = 2)
-    offsets=seq(0,1,length.out=nrow(fit$result))
+  if (all(is.na(fit$timeAxis))) {
+    Plotting.Utils::pretty_plot(c(0, 1), c(0, 100), ylab="Relative area (%)", xlab="Time", axes=2)
+    offsets <- seq(0, 1, length.out=nrow(fit$result))
   } else {
-    Plotting.Utils::pretty_plot(range(fit$timeAxis),c(0,100),ylab = "Relative area (%)", xlab = 'Time / h', axes = c(1,2))
-    offsets=fit$timeAxis
+    Plotting.Utils::pretty_plot(
+      range(fit$timeAxis), c(0, 100),
+      ylab="Relative area (%)",
+      xlab="Time / h",
+      axes=c(1, 2)
+    )
+    offsets <- fit$timeAxis
   }
 
-  #plot areas
+  # plot areas
 
-  total_integral=fit$result[,'fit_integrated_area']
-  max_integral=max(total_integral)
+  total_integral <- fit$result[, "fit_integrated_area"]
+  max_integral <- max(total_integral)
 
-  for(m in 1:length(fit$models)) {
-
-    integrals=fit$result[,paste0('integrated_area_',m)]
-    stderrs=fit$result[,paste0('std.error_',m)]*(fit$integration_range[[2]]-fit$integration_range[[1]])
-    if(NAasZero) {
-      integrals[is.na(integrals)]<-0
-      stderrs[is.na(stderrs)]<-0
+  for (m in 1:length(fit$models)) {
+    integrals <- fit$result[, paste0("integrated_area_", m)]
+    stderrs <- fit$result[, paste0("std.error_", m)] * (fit$integration_range[[2]] - fit$integration_range[[1]])
+    if (NAasZero) {
+      integrals[is.na(integrals)] <- 0
+      stderrs[is.na(stderrs)] <- 0
     }
 
-    int_min=integrals-stderrs
-    int_max=integrals+stderrs
-    points(offsets,integrals/max_integral * 100,pch=pch,col=deconv_cols[[m]])
+    int_min <- integrals - stderrs
+    int_max <- integrals + stderrs
+    points(offsets, integrals / max_integral * 100, pch=pch, col=deconv_cols[[m]])
 
-    #errors
-    if(showErrors) {
-      col1=adjustcolor(deconv_cols[[m]], alpha.f=error_alpha)
-      xpoints=c(offsets,rev(offsets))
-      ypoints= c(int_max,rev(int_min))
-      polygon(xpoints[!is.na(ypoints)],ypoints[!is.na(ypoints)]/max_integral * 100,col=col1,border='NA')
-    }
-  }
-
-  if(plot_total_integral) {
-    points(offsets,total_integral/max_integral * 100,pch=pch,col=fit_col)
-    if(showErrors) {
-      col1=adjustcolor(fit_col, alpha.f=error_alpha)
-
-      xpoints=c(offsets,rev(offsets))
-      sde=fit$result[,'std.error']*(fit$integration_range[[2]]-fit$integration_range[[1]])
-
-      int_min=total_integral-sde
-      int_max=total_integral+sde
-
-      ypoints= c(int_max,rev(int_min))
-      polygon(xpoints[!is.na(ypoints)],ypoints[!is.na(ypoints)]/max_integral * 100,col=col1,border='NA')
+    # errors
+    if (showErrors) {
+      col1 <- adjustcolor(deconv_cols[[m]], alpha.f=error_alpha)
+      xpoints <- c(offsets, rev(offsets))
+      ypoints <- c(int_max, rev(int_min))
+      polygon(xpoints[!is.na(ypoints)], ypoints[!is.na(ypoints)] / max_integral * 100, col=col1, border="NA")
     }
   }
-  if(showLegend) {
-    leg = sapply(fit$models, function(x) x$name)
-    leg = c('Total', leg)
-    cols = deconv_cols[1:length(fit$models)]
-    cols = c(fit_col, cols)
-    legend('top', horiz=T, inset=-0.1, xpd=NA, leg, col = cols, pch=pch)
+
+  if (plot_total_integral) {
+    points(offsets, total_integral / max_integral * 100, pch=pch, col=fit_col)
+    if (showErrors) {
+      col1 <- adjustcolor(fit_col, alpha.f=error_alpha)
+
+      xpoints <- c(offsets, rev(offsets))
+      sde <- fit$result[, "std.error"] * (fit$integration_range[[2]] - fit$integration_range[[1]])
+
+      int_min <- total_integral - sde
+      int_max <- total_integral + sde
+
+      ypoints <- c(int_max, rev(int_min))
+      polygon(xpoints[!is.na(ypoints)], ypoints[!is.na(ypoints)] / max_integral * 100, col=col1, border="NA")
+    }
+  }
+  if (showLegend) {
+    leg <- sapply(fit$models, function(x) x$name)
+    leg <- c("Total", leg)
+    cols <- deconv_cols[1:length(fit$models)]
+    cols <- c(fit_col, cols)
+    legend("top", horiz=T, inset=-0.1, xpd=NA, leg, col=cols, pch=pch)
   }
 }
