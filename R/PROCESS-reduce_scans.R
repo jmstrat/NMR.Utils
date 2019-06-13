063119d0bf244e5f5318d6b06da0b35c099ed8f7
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-13 20:20:13 +0100

    lint

--- R/PROCESS-reduce_scans.R
+++ R/PROCESS-reduce_scans.R
@@ -5,17 +5,19 @@
 #' @return An idential data frame whose scans are limited to those in scan_range
 #' @export
 #' @examples
-#' reduceScans(spectra,c(1,50))
-reduceScans <-function(spectra,scan_range) {
-  if(missing(scan_range)) scan_range <- c(1, find_last_scan(spectra))
-  jms.classes::log.info('Removing scans outside of range %s - %s', scan_range[[1]], scan_range[[2]])
-  scan_range=scan_range+1
-  if(scan_range[[1]]<2)
-    scan_range[[1]]=2
-  n=ncol(spectra)
-  if(scan_range[[2]]>n)
-    scan_range[[2]]=n
-  spectra[,c(1,scan_range[[1]]:scan_range[[2]])]
+#' reduceScans(spectra, c(1, 50))
+reduceScans <- function(spectra, scan_range) {
+  if (missing(scan_range)) scan_range <- c(1, find_last_scan(spectra))
+  jms.classes::log.info("Removing scans outside of range %s - %s", scan_range[[1]], scan_range[[2]])
+  scan_range <- scan_range + 1
+  if (scan_range[[1]] < 2) {
+    scan_range[[1]] <- 2
+  }
+  n <- ncol(spectra)
+  if (scan_range[[2]] > n) {
+    scan_range[[2]] <- n
+  }
+  spectra[, c(1, scan_range[[1]]:scan_range[[2]])]
 }
 
 #' Finds the number of the last (non-empty) scan
@@ -27,8 +29,10 @@ reduceScans <-function(spectra,scan_range) {
 #' @examples
 #' find_last_scan(spectra)
 find_last_scan <- function(spectra, tol=0.01) {
-  if(any_complex(spectra)) spectra = makeReal(spectra)
+  if (any_complex(spectra)) spectra <- makeReal(spectra)
   not_missing_scans <- which(!sapply(spectra, function(y) abs(max(y) - min(y)) < tol))
-  if(length(not_missing_scans)) return(not_missing_scans[[length(not_missing_scans)]]-1)
+  if (length(not_missing_scans)) {
+    return(not_missing_scans[[length(not_missing_scans)]] - 1)
+  }
   0
 }
